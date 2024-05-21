eb6b1d810ee4907fd1832d82c9ff6b81e9c39966
CheesyChilly <97239118+CheesyChilly@users.noreply.github.com>
2024-05-22 00:13:18 +0530

    added basics

--- OnlineMarket/OnlineMarket/urls.py
+++ OnlineMarket/OnlineMarket/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from core.views import index, contact
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", index, name="index"),
+    path("contact/", contact, name="contact"),
+    path("admin/", admin.site.urls),
 ]
