6234a11b7ab75e45c327bf8827ba104d3ab96e8a
CheesyChilly <97239118+CheesyChilly@users.noreply.github.com>
2024-05-25 19:44:16 +0530

    added some more functionalities

--- OnlineMarket/item/views.py
+++ OnlineMarket/item/views.py
@@ -1,7 +1,8 @@
-from unicodedata import category
-from django.shortcuts import render, get_object_or_404
-from .models import Item
 
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item
+from .forms import NewItemForm, EditItemForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -12,4 +13,52 @@ def detail(request, pk):
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items,
+    })
+
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New item',
+    })
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
     })
